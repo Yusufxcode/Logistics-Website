a7889ee468bb45a6fe82dc75fef19b51a5aef995
Adebakin Yusuf <109008836+Yusufxcode@users.noreply.github.com>
2022-11-14 08:18:22 +0100

    mobile toggle

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-var arr=[10,9,7,8];  
-var rev=arr.reverse();  
-document.writeln(rev);  
-console.log(rev);
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", ()=> {
+    const visibility = primaryNav.getAttribute('data-visible');
+    
+    if (visibility === "false"){
+        primaryNav.setAttribute('data-visible', true)
+        navToggle.setAttribute('aria-expanded', true)
+    }else if (visibility === "true"){
+        primaryNav.setAttribute('data-visible', false)
+        navToggle.setAttribute('aria-expanded', true)
+    }
+})
